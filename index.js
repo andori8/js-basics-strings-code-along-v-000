5a0cea749ec36c8f938fbf383cc6a9d5ec1b53f4
Ryan Do <falcsmike7@gmail.com>
2017-10-26 21:45:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // Write your code in this file!
 const currentUser = 'Ryan Do';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
